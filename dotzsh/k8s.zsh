3f79d57c91616acf71e07df5514223ec59558f79
Li Chen <mail@chen.li>
2019-12-13 15:35:03 -0500

    Add custom kc prompt

--- dotzsh/k8s.zsh
+++ dotzsh/k8s.zsh
@@ -1,3 +1,69 @@
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+export KUBECTL_CONTEXT="dev-eks-02"
+export KUBECTL_NAMESPACE="hinge-services"
 
-alias kc=kubectl
+alias kc='kubectl --context="$KUBECTL_CONTEXT" --namespace="$KUBECTL_NAMESPACE"'
+
+# unset
+kubectl config unset current-context > /dev/null
+
+# Usage: kc-ctx [context]
+kc-ctx() {
+    local yellow=$(tput setaf 3 || true)
+    local darkbg=$(tput setab 0 || true)
+    local normal=$(tput sgr0 || true)
+
+    if [ $# -eq 0 ]; then
+        local current_ctx=$(kubectl config view -o=jsonpath='{.current-context}')
+        local ctx_list=$(kubectl config get-contexts -o=name | sort -n)
+
+        for c in $ctx_list; do
+            if [ "$KUBECTL_CONTEXT" = "$c" ]; then
+                echo "${darkbg}${yellow}${c}${normal}"
+            else
+                echo $c
+            fi
+        done
+    else
+        if kubectl config get-contexts -o=name | grep -q "^${1}$"; then
+            export KUBECTL_CONTEXT=$1
+            echo "Switched to context: $1"
+        else
+            echo "Invalid context: $1"
+        fi
+    fi
+}
+
+# Usage: kc-ctx-renamce old new
+kc-ctx-rename() {
+    if [ $# -ne 2 ]; then
+        echo "kc-ctx-rename old new"
+    else
+        kubectx config rename-context "$1" "$2"
+    fi
+}
+
+# Usage: kc-ns [namespace]
+kc-ns() {
+    local yellow=$(tput setaf 3 || true)
+    local darkbg=$(tput setab 0 || true)
+    local normal=$(tput sgr0 || true)
+
+    if [ $# -eq 0 ]; then
+        for n in $(kubectl --context="$KUBECTL_CONTEXT" get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}'); do
+            if [ "$KUBECTL_NAMESPACE" = "$n" ]; then
+                echo "${darkbg}${yellow}${n}${normal}"
+            else
+                echo $n
+            fi
+        done
+    else
+        if kubectl --context="$KUBECTL_CONTEXT" get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}' | grep -q "^${1}$"; then
+            export KUBECTL_NAMESPACE=$1
+            echo "Switched to namespace: $1"
+        else
+            echo "Invalid namespace: $1"
+        fi
+    fi
+}
+
+
--- dotzsh/powerlevel9k.zsh
+++ dotzsh/powerlevel9k.zsh
@@ -7,7 +7,15 @@ fi
 ZSH_THEME="powerlevel9k/powerlevel9k"
 POWERLEVEL9K_MODE="awesome-fontconfig"
 
+prompt_kc() {
+    echo -en "\u2388 $KUBECTL_CONTEXT/$KUBECTL_NAMESPACE"
+}
+
+POWERLEVEL9K_CUSTOM_KC="prompt_kc"
+POWERLEVEL9K_CUSTOM_KC_BACKGROUND="magenta"
+POWERLEVEL9K_CUSTOM_KC_FOREGROUND="white"
+
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
 POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_right"
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context aws kubecontext newline dir vcs)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context aws custom_kc newline dir vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs)
