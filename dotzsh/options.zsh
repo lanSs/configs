921dc7034f10a1c5ebf9b90adf320d9be55cb572
Li Chen <mail@chen.li>
2019-12-15 00:09:29 -0500

    Add history options in zsh

--- dotzsh/options.zsh
+++ dotzsh/options.zsh
@@ -1,8 +1,14 @@
 setopt correct
 setopt hash_cmds
 setopt hash_dirs
+
+setopt bang_hist
+setopt extended_history
+setopt inc_append_history
+setopt share_history
 setopt hist_ignore_all_dups
 setopt hist_find_no_dups
+setopt hist_save_no_dups
 setopt extended_glob
 
 # Enable split string on space
