b8c2a382c3faf205ecf96af095404cd19cfa5c34
Li Chen <mail@chen.li>
2019-12-16 16:36:14 -0500

    Add search history arrow

--- dotzsh/bindkey.zsh
+++ dotzsh/bindkey.zsh
@@ -7,3 +7,6 @@ bindkey "\e[4~" end-of-line
 bindkey "\e[8~" end-of-line
 
 bindkey "\e[3~" delete-char
+
+bindkey '^[[A' up-line-or-search                                                
+bindkey '^[[B' down-line-or-search
