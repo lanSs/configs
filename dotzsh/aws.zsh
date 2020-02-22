35dca56e85bc662b460dfa2687fac81b0699e11e
Li Chen <mail@chen.li>
2020-02-21 23:40:54 -0500

    Add region in aws prompt

--- dotzsh/aws.zsh
+++ dotzsh/aws.zsh
@@ -1,14 +1,43 @@
 export AWS_DEFAULT_PROFILE="hinge-dev"
 export AWS_PROFILE=$AWS_DEFAULT_PROFILE
+export AWS_DEFAULT_REGION="us-east-1"
 
 # keep varibles between session
 AWS_PROFILE_TMP="/tmp/aws_profile"
+AWS_REGION_TMP="/tmp/aws_region"
 
 if [ -f $AWS_PROFILE_TMP ]; then
     VALUE=$(<$AWS_PROFILE_TMP)
     export AWS_DEFAULT_PROFILE=$VALUE
     export AWS_PROFILE=$VALUE
 fi
+
+if [ -f $AWS_REGION_TMP ]; then
+    VALUE=$(<$AWS_REGION_TMP)
+    export AWS_DEFAULT_REGION=$VALUE
+fi
+
+declare -A aws_region_shortname
+aws_region_shortname=(
+    [us-east-1]="ue1"
+    [us-east-2]="ue2"
+    [us-west-1]="uw1"
+    [us-west-2]="uw2"
+    [ap-east-1]="ae1"
+    [ap-south-1]="as1"
+    [ap-northeast-2]="an2"
+    [ap-southeast-1]="as1"
+    [ap-southeast-2]="as2"
+    [ap-northeast-1]="an1"
+    [ca-central-1]="cc1"
+    [eu-central-1]="ec1"
+    [eu-west-1]="ew1"
+    [eu-west-2]="ew2"
+    [eu-west-3]="ew3"
+    [eu-north-1]="en1"
+    [me-south-1]="ms1"
+    [sa-east-1]="se1"
+)
 
 #  Usage: aws-instances [filter]
 aws-instances() {
@@ -36,12 +65,14 @@ aws-elb-ips() {
 aws-connect() {
     if [ $# -ne 1 ]; then
         echo "aws-connect instance-id"
-    else
+    elif [[ $1 == i-* ]]; then
         aws ssm start-session --target $1
+    else
+        echo "Invalid instance id"
     fi
 }
 
-# Usage: aws-profile [name]
+# Usage: aws-profile [name] [region]
 aws-profile() {
     local yellow darkbg normal
     yellow=$(tput setaf 3 || true)
@@ -68,5 +99,18 @@ aws-profile() {
         else
             echo "Invalid AWS profile: $1"
         fi
+
+        # region is provided
+        if ! [ -z $2 ]; then
+            if ! [ -z $aws_region_shortname[$2] ]; then
+                echo "Using region: $2"
+                export AWS_DEFAULT_REGION=$2
+                echo -n $2 > $AWS_REGION_TMP
+            else
+                echo "Invalid region: $2, ignored"
+            fi
+        fi
     fi
 }
+
+
--- dotzsh/powerlevel9k.zsh
+++ dotzsh/powerlevel9k.zsh
@@ -7,7 +7,15 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     source /usr/local/opt/powerlevel9k/powerlevel9k.zsh-theme
 fi
 
+prompt_aws() {
+    echo -en "\uE895 [$aws_region_shortname[$AWS_DEFAULT_REGION]]$AWS_PROFILE"
+}
+
+POWERLEVEL9K_CUSTOM_AWS="prompt_aws"
+POWERLEVEL9K_CUSTOM_AWS_BACKGROUND="red"
+POWERLEVEL9K_CUSTOM_AWS_FOREGROUND="white"
+
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
 POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_right"
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context aws kubecontext newline dir vcs)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context custom_aws kubecontext newline dir vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs)
