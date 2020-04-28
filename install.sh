6e0bc7bc6a268397c3e4e35ec0324fabf115e376
Li Chen <mail@chen.li>
2020-04-28 11:15:17 -0400

    install.sh installs the fonts on mac os and nord-iterm2 as a submodule

--- install.sh
+++ install.sh
@@ -25,8 +25,24 @@ function remove() {
 
 if [ "$1" = "install" ]; then
     for f in $(find . -maxdepth 1 -name "dot*"); do
-        install $(pwd) $(basename $f)  
+        install $(pwd) $(basename $f) 
     done
+
+    echo "Installing & updating the submodules"
+    git submodule init
+    git submodule update
+
+    unameOut="$(uname -s)"
+    if [ "$unameOut" = "Darwin" ]; then
+        echo "Installing fonts"
+        cp fonts/Droid+Sans+Mono+Awesome.ttf ~/Library/Fonts/Droid+Sans+Mono+Awesome.ttf
+        cp fonts/Inconsolata+Awesome.ttf ~/Library/Fonts/Inconsolata+Awesome.ttf
+        cp fonts/SourceCodePro+Powerline+Awesome+Regular.ttf ~/Library/Fonts/SourceCodePro+Powerline+Awesome+Regular.ttf
+    else
+        echo "MANUAL ACTION REQUIRED: please install the fonts in the fonts folder"
+    fi
+    echo "MANUAL ACTION REQUIRED: implement the steps in the nord-iterm2/README.md to finalise the styling"
+
 elif [ "$1" = "uninstall" ]; then
     for f in $(find . -maxdepth 1 -name "dot*"); do
         remove $(basename $f)  
